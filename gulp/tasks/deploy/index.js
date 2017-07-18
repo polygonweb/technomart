b152407f48991d7b77c9058e4a76effe79b4054c
monochromer <monochromer@mail.ru>
2017-07-18 16:17:09 +0500

    deploy

--- gulp/tasks/deploy/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Публикация сборки проекта в git-репозитории
- */
-module.exports = (gulp, plugins, config) => (done) => {
-  return plugins.combiner(
-      gulp.src(config.src),
-      plugins.deployGit(config.deployOptions)
-    ).on('error', config.onError);
-};
