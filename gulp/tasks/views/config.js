00749e0c432b94b99acd16493f95dd30349a4eba
monochromer <monochromer@mail.ru>
2017-08-17 02:33:11 +0500

    изменение обработки шаблонов

--- gulp/tasks/views/config.js
+++ gulp/tasks/views/config.js
@@ -2,11 +2,22 @@ module.exports = {
   taskName: 'views',
   description: 'Обработка html',
   isProduction: process.env.NODE_ENV === 'production',
-  src: 'src/views/pages/*.*',
+  src: 'src/views/pages/*/*.pug',
   watchFiles: 'src/views/**/*.*',
   dest: 'build/',
+  dataPath: '',
   engineOptions: {
     locals: {},
-    pretty: '\t'
+    pretty: '\t',
+    basedir: 'src/views/'
+  },
+  prettify: {
+    indent_size: 4,
+    indent_char: ' ',
+    wrap_attributes: 'auto', // 'force'
+    preserve_newlines: false,
+    indent_inner_html: true,
+    // unformatted: [], //  ['pre', 'code']
+    end_with_newline: true
   }
 }
--- gulp/tasks/views/index.js
+++ gulp/tasks/views/index.js
@@ -2,8 +2,22 @@
  * Обработка представлений
  */
 module.exports = (gulp, plugins, config) => (done) => {
+  if (config.dataPath) {
+    let dataPath = config.dataPath;
+    delete require.cache[require.resolve(dataPath)];
+    let data = require(dataPath);
+    Object.assign(config.engineOptions, {
+      locals: data
+    });
+  }
+
   return plugins.combiner(
     gulp.src(config.src),
+    plugins.rename(path => {
+      let { dirname, basename } = path;
+      path.basename = basename === 'index' ? dirname : `${dirname}-${basename}`;
+      path.dirname = '.';
+    }),
     plugins.pug(config.engineOptions),
     gulp.dest(config.dest),
     // plugins.if(
@@ -11,6 +25,11 @@ module.exports = (gulp, plugins, config) => (done) => {
     //     plugins.browserSync.stream({
     //       once: true
     //     }),
+    //     plugins.util.noop()
+    // )
+    // plugins.if(
+    //   !config.isProduction,
+    //     plugins.prettify(config.prettify),
     //     plugins.util.noop()
     // )
   ).on('error', config.onError);
